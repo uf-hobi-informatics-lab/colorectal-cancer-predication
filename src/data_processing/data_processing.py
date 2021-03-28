8f9e6d9eb05e290ce8dd14dea4cf0534c38de55a
Chong Dang <chong.dang@gatech.edu>
2021-03-27 23:09:55 -0400

    fix bugs

--- src/data_processing/data_processing.py
+++ src/data_processing/data_processing.py
@@ -14,7 +14,7 @@ from common_utils.utils import pkl_dump,pkl_load
 from sklearn.model_selection import train_test_split
 
 def load_data(features_path,features_filename,data_filename):
-    fea2id, features = pkl_load(f"{features_filename}", features_path)
+    fea2id = pkl_load(f"{features_filename}", features_path)
     data = pkl_load(f"{data_filename}", features_path)
     return fea2id,data
 
@@ -28,17 +28,16 @@ def convert_to_dataframe(fea2id,patient_data):
                 patient_with_features.loc[i,j]=1
             else:
                 patient_with_features.loc[i,j]=0
+    return patient_with_features
 
 def main(args):
 
-    case_control_path = args.case_control_path
-    case_control_filename = args.case_control_filename
     features_path = args.features_path
     features_filename = args.features_filename
     data_filename = args.data_filename
     output_path = args.output_path
 
-    ptIDs,fea2id,patient_data = load_data(features_path,features_filename,data_filename)
+    fea2id,patient_data = load_data(features_path,features_filename,data_filename)
     print('Load Data Done!')
     patient_with_features = convert_to_dataframe(fea2id,patient_data)
     print('Finished!')
