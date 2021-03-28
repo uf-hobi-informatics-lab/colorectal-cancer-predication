e9344d60eaa80f40e5cda5966e5566b083ef524a
Chong Dang <chong.dang@gatech.edu>
2021-03-27 22:58:47 -0400

    update run.sh

--- src/data_processing/run.sh
+++ src/data_processing/run.sh
@@ -6,4 +6,4 @@ python data_processing.py \
   --features_path '/mnt/data1/chong/2021-CRC/updated_data/encoding_files/' \
   --features_filename 'data_CC0yr_expr_features.pkl' \
   --data_filename 'data_CC0yr_expr.pkl' \
-  --output_path '/mnt/data1/chong/2021-CRC/updated_data/' \
+  --output_path '/mnt/data1/chong/2021-CRC/updated_data/'
