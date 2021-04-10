c16b8d20b01cd918ba42d92f3f95f0d424b5782c
Chong Dang <chong.dang@gatech.edu>
2021-04-10 00:44:54 -0400

    10 times tuning result

--- src/training_pipeline/training.py
+++ src/training_pipeline/training.py
@@ -6,6 +6,8 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='(%(asctime)s) [%(process)d] %(levelname)s: %(message)s')
 
 import argparse
+import scipy.stats
+import numpy as np
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -17,7 +19,7 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RandomizedSearchCV, cross_val_score, StratifiedKFold, cross_val_predict, train_test_split
-from sklearn.metrics import roc_auc_score, average_precision_score, roc_curve, auc, precision_recall_fscore_support
+from sklearn import metrics
 
 from .config import ModelType, lr_tuned_parameters, rf_tuned_parameters, svm_tuned_parameters, gbdt_tuned_parameters
 
@@ -25,45 +27,86 @@ import sys
 sys.path.append("..")
 from common_utils.utils import pkl_dump,pkl_load
 
-def run_experiment(clf, params, task, nb, nit, model_type, train_test_path, output_path):
-    print(f"current task: {task} {model_type}")
-    model_dump = f"{task}year_{model_type}_model.pkl"
+# def run_experiment(clf, params, task, nb, nit, model_type, train_test_path, output_path):
+#     print(f"current task: {task} {model_type}")
+#     model_dump = f"{task}year_{model_type}_model.pkl"
 
-    tr_dx, tr_dy, ts_dx, ts_dy = pkl_load(f"expr_data_CC{task}yr_train_test.pkl", train_test_path)
+#     tr_dx, tr_dy, ts_dx, ts_dy = pkl_load(f"expr_data_CC{task}yr_train_test.pkl", train_test_path)
     
-    cv_model = RandomizedSearchCV(clf, params, scoring='roc_auc', n_jobs=nb, 
-                                    cv=StratifiedKFold(n_splits=5, shuffle=True), 
-                                    verbose=1, n_iter=nit)
-    cv_model.fit(tr_dx, tr_dy)
-    opt_clf = cv_model.best_estimator_
-    pkl_dump(opt_clf, model_dump, output_path)
+#     cv_model = RandomizedSearchCV(clf, params, scoring='roc_auc', n_jobs=nb, 
+#                                     cv=StratifiedKFold(n_splits=5, shuffle=True), 
+#                                     verbose=1, n_iter=nit)
+#     cv_model.fit(tr_dx, tr_dy)
+#     opt_clf = cv_model.best_estimator_
+#     pkl_dump(opt_clf, model_dump, output_path)
 
-    preds = opt_clf.predict_proba(ts_dx)
-    pkl_dump(preds, f"{task}year_{model_type}_preds.pkl", output_path)
+#     preds = opt_clf.predict_proba(ts_dx)
+#     pkl_dump(preds, f"{task}year_{model_type}_preds.pkl", output_path)
 
-    idx = np.argmax(opt_clf.classes_)
-    preds_1 = list(map(lambda x: x[idx], preds))
+#     idx = np.argmax(opt_clf.classes_)
+#     preds_1 = list(map(lambda x: x[idx], preds))
 
-    auc_score = roc_auc_score(ts_dy, preds_1)
-    fprs, tprs, ths = roc_curve(ts_dy, preds_1)
-    print("auc_score is : ",auc_score)
+#     auc_score = roc_auc_score(ts_dy, preds_1)
+#     fprs, tprs, ths = roc_curve(ts_dy, preds_1)
+#     print("auc_score is : ",auc_score)
     
-    J_idx = np.argmax(tprs - fprs)
-    fpr, tpr, th = fprs[J_idx], tprs[J_idx], ths[J_idx]
-    auc_score1 = auc(fprs, tprs)
+#     J_idx = np.argmax(tprs - fprs)
+#     fpr, tpr, th = fprs[J_idx], tprs[J_idx], ths[J_idx]
+#     auc_score1 = auc(fprs, tprs)
 
-    sen = tpr
-    spe = 1 - fpr
-    stats = [sen, spe, auc_score1]
+#     sen = tpr
+#     spe = 1 - fpr
+#     stats = [sen, spe, auc_score1]
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return m, m-h, m+h
+
+def print_with_ci(performance, name):
+    m, m_l, m_h = mean_confidence_interval(performance[name])
+    print('%s: %.3f (%.3f, %.3f)'%(name, m, m_l, m_h))
+
+def run_experiment(X, y, clf, run_times, train_test_path, output_path):
+    
+    performance = pd.DataFrame()
+
+    metrics_average = 'binary'
+    labels = None #[0]
+    for i in range(run_times):
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=i)
+
+        clf.fit(X_train,y_train.values.ravel())
+        y_pred=clf.predict(X_test)
+        y_prob=clf.predict_proba(X_test)[:,1]
+
+        performance = performance.append({
+                'i': i,
+                'f1': metrics.f1_score(y_test, y_pred, average=metrics_average, labels = labels),
+                'precision': metrics.precision_score(y_test, y_pred, average=metrics_average, labels = labels),
+                'recall': metrics.recall_score(y_test, y_pred, average=metrics_average, labels = labels),
+                'auc_roc': metrics.roc_auc_score(y_test, y_prob)
+            }, ignore_index=True)
+
+    print("--------------------------------------------------")
+    print_with_ci(performance, 'f1')
+    print_with_ci(performance, 'precision')
+    print_with_ci(performance, 'recall')
+    print_with_ci(performance, 'auc_roc')
+    print("--------------------------------------------------")
 
 def main(args):
 
     train_test_path = args.train_test_path
     output_path = args.output_path
     model_type = args.model_type
-    task = args.predication_window
-    nb = args.number_of_jobs
-    nit = args.n_iterations
+    run_times = args.run_times
+
+    X = data.iloc[:,2:]
+    y = data.iloc[:,1:2]
 
     if model_type is ModelType.M_LR:
         clf = LogisticRegression(warm_start=True)
@@ -75,12 +118,11 @@ def main(args):
     elif model_type is ModelType.M_SVM:
         clf = svm.SVC(probability=True)
         tuned_parameters = svm_tuned_parameters
-
     else:
         clf = GradientBoostingClassifier()
         tuned_parameters = gbdt_tuned_parameters
 
-    run_experiment(clf, tuned_parameters, task, nb, nit, model_type, train_test_path, output_path)
+    run_experiment(X, y, clf, run_times, train_test_path, output_path)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -97,17 +139,9 @@ if __name__ == '__main__':
     parser.add_argument("--model_type", required=True, type=str, 
         help="the model type")
 
-    # predication_window
-    parser.add_argument("--predication_window", required=True, type=int, 
-        help="the predication window, 0 year, 1 year, 3 years, 5 years")
-
-    # number of jobs
-    parser.add_argument("--number_of_jobs", required=True, type=int,
-        help="the nb")
-
-    # n_iterations
-    parser.add_argument("--n_iterations", required=True, type=int, 
-        help="the number of iterations")
+    # run_times
+    parser.add_argument("--run_times", required=True, type=int, 
+        help="the running time for calculating confidence interval")
 
     args = parser.parse_args()
     main(args)
